6eba10bd986cdc4f6e1305e2ce974fe5bb9e48c4
Rhavyx <everluca@outlook.com>
2021-09-18 18:57:17 -0300

    :sparkles: 50 patterns of shaders

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -312,7 +312,23 @@ void main()
     //gl_FragColor = vec4(strength, strength, strength, 1.0); 
     
     //Pattern 46
-    float strength = cnoise(vuv * 10.0);
+    //float strength = cnoise(vuv * 10.0);
+    //gl_FragColor = vec4(strength, strength, strength, 1.0); 
+
+    //Pattern 47
+    //float strength = step(0.0, cnoise(vuv * 10.0));
+    //gl_FragColor = vec4(strength, strength, strength, 1.0); 
+
+    //Pattern 48
+    //float strength = 1.0 - abs(cnoise(vuv * 10.0));
+    //gl_FragColor = vec4(strength, strength, strength, 1.0); 
+
+    //Pattern 49
+    //float strength = sin(cnoise(vuv * 10.0) * 40.0);
+    //gl_FragColor = vec4(strength, strength, strength, 1.0); 
+
+    //Pattern 50
+    float strength = step(0.9, sin(cnoise(vuv * 10.0) * 20.0));
     gl_FragColor = vec4(strength, strength, strength, 1.0); 
 
 }
